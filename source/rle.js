3759f4bb9fdc71122b7b04122f2b0b2b3ae0cf3c
Artem Doctor <artyomd2009@yandex.ru>
2019-04-04 00:04:10 +0300

    Update rle.js

--- source/rle.js
+++ source/rle.js
@@ -1,20 +1,19 @@
 'use strict';
 
-const addAmount = (output, amount) => amount > 1 ? output + amount : output
+const addAmount = (output, amount) => amount > 1 ? output + amount : output;
 
 const rle = (text) => {
 	if (typeof text !== "string") {
 		return;
 	}
 	let output = text.substr(0, 1);
-	let amount = [...text].reduce((amount, curr) => {
+	const lastAmount = [...text].reduce((amount, curr) => {
 		if (curr !== output.substr(-1, 1)) {
-			output = addAmount(output, amount) + curr
+			output = addAmount(output, amount) + curr;
 			amount = 0;
 		}
 		return ++amount;
 	}, 0);
-	output = addAmount(output, amount)
 	
-	return output;
+	return addAmount(output, lastAmount);
 }
